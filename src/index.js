b0d9c2d4d2e8e60415dab1d6f048d73ad7f2445d
ygzy <ygzg@amazon.com>
2016-09-27 17:39:58 -0700

    minor change

--- src/index.js
+++ src/index.js
@@ -26,7 +26,8 @@ var languageString = {
             "HELP_UNHANDLED": "Say yes to continue, or no to end the game.",
             "START_UNHANDLED": "Say start to start a new game.",
             "NEW_GAME_MESSAGE": "Welcome to %s. ",
-            "WELCOME_MESSAGE": "I will ask you %s questions, try to get as many right as you can. Just say the number of the answer. Let\'s begin. ",
+            "WELCOME_MESSAGE": "I will ask you %s questions, try to get as many right as you can. " +
+            "Just say the number of the answer. Let\'s begin. ",
             "ANSWER_CORRECT_MESSAGE": "correct. ",
             "ANSWER_WRONG_MESSAGE": "wrong. ",
             "CORRECT_ANSWER_MESSAGE": "The correct answer is %s: %s. ",
@@ -50,8 +51,10 @@ var languageString = {
             "NO_MESSAGE": "Ok, we\'ll play another time. Goodbye!",
             "TRIVIA_UNHANDLED": "Try saying a number between 1 and %s",
             "HELP_UNHANDLED": "Say yes to continue, or no to end the game.",
+            "START_UNHANDLED": "Say start to start a new game.",
             "NEW_GAME_MESSAGE": "Welcome to %s. ",
-            "WELCOME_MESSAGE": "I will ask you %s questions, try to get as many right as you can. Just say the number of the answer. Let\'s begin. ",
+            "WELCOME_MESSAGE": "I will ask you %s questions, try to get as many right as you can. " +
+            "Just say the number of the answer. Let\'s begin. ",
             "ANSWER_CORRECT_MESSAGE": "correct. ",
             "ANSWER_WRONG_MESSAGE": "wrong. ",
             "CORRECT_ANSWER_MESSAGE": "The correct answer is %s: %s. ",
@@ -69,12 +72,13 @@ var languageString = {
             "Sage beispielsweise eins, zwei, drei oder vier. Du kannst jederzeit ein neues Spiel beginnen, sage einfach „Spiel starten“. ",
             "REPEAT_QUESTION_MESSAGE": "Wenn die letzte Frage wiederholt werden soll, sage „Wiederholen“ ",
             "ASK_MESSAGE_START": "Möchten Sie beginnen?",
-            "HELP_REPROMPT": "Wenn du eine Frage beantworten willst, antworte mit der Zahl, die zur richtigen Antwort gehört. Möchtest du weiterspielen?",
+            "HELP_REPROMPT": "Wenn du eine Frage beantworten willst, antworte mit der Zahl, die zur richtigen Antwort gehört. ",
             "STOP_MESSAGE": "Möchtest du weiterspielen?",
             "CANCEL_MESSAGE": "OK, dann lass uns bald mal wieder spielen.",
             "NO_MESSAGE": "OK, spielen wir ein andermal. Auf Wiedersehen!",
             "TRIVIA_UNHANDLED": "Sagt eine Zahl beispielsweise zwischen 1 und %s",
             "HELP_UNHANDLED": "Sage ja, um fortzufahren, oder nein, um das Spiel zu beenden.",
+            "START_UNHANDLED": "Du kannst jederzeit ein neues Spiel beginnen, sage einfach „Spiel starten“.",
             "NEW_GAME_MESSAGE": "Willkommen bei %s. ",
             "WELCOME_MESSAGE": "Ich stelle dir %s Fragen und du versuchst, so viele wie möglich richtig zu beantworten. " +
             "Sage einfach die Zahl, die zur richtigen Antwort passt. Fangen wir an. ",
@@ -105,19 +109,19 @@ var newSessionHandlers = {
     /**
      * Entry point. Start a new game on new session. Handle any setup logic here.
      */
-    'LaunchRequest': function () {
+    "LaunchRequest": function () {
         this.handler.state = GAME_STATES.START;
         this.emitWithState("StartGame", true);
     },
-    'AMAZON.StartOverIntent': function() {
+    "AMAZON.StartOverIntent": function() {
         this.handler.state = GAME_STATES.START;
         this.emitWithState("StartGame", true);
     },
-    'AMAZON.HelpIntent': function() {
+    "AMAZON.HelpIntent": function() {
         this.handler.state = GAME_STATES.HELP;
-        this.emitWithState('helpTheUser', true);
+        this.emitWithState("helpTheUser", true);
     },
-    'Unhandled': function () {
+    "Unhandled": function () {
         var speechOutput = this.t("START_UNHANDLED");
         this.emit(":ask", speechOutput, speechOutput);
     }
@@ -202,25 +206,25 @@ var helpStateHandlers = Alexa.CreateStateHandler(GAME_STATES.HELP, {
         var repromptText = this.t("HELP_REPROMPT") + askMessage;
         this.emit(":ask", speechOutput, repromptText);
     },
-    'AMAZON.StartOverIntent': function () {
+    "AMAZON.StartOverIntent": function () {
         this.handler.state = GAME_STATES.START;
-        this.emitWithState('StartGame', false);
+        this.emitWithState("StartGame", false);
     },
     "AMAZON.RepeatIntent": function () {
-        var newGame = (this.attributes['speechOutput'] && this.attributes['repromptText']) ? false : true;
+        var newGame = (this.attributes["speechOutput"] && this.attributes["repromptText"]) ? false : true;
         this.emitWithState("helpTheUser", newGame);
     },
     "AMAZON.HelpIntent": function() {
-        var newGame = (this.attributes['speechOutput'] && this.attributes['repromptText']) ? false : true;
+        var newGame = (this.attributes["speechOutput"] && this.attributes["repromptText"]) ? false : true;
         this.emitWithState("helpTheUser", newGame);
     },
     "AMAZON.YesIntent": function() {
-        if (this.attributes['speechOutput'] && this.attributes['repromptText']) {
+        if (this.attributes["speechOutput"] && this.attributes["repromptText"]) {
             this.handler.state = GAME_STATES.TRIVIA;
-            this.emitWithState('AMAZON.RepeatIntent');
+            this.emitWithState("AMAZON.RepeatIntent");
         } else {
             this.handler.state = GAME_STATES.START;
-            this.emitWithState('StartGame', false);
+            this.emitWithState("StartGame", false);
         }
     },
     "AMAZON.NoIntent": function() {
--- src/questions.js
+++ src/questions.js
@@ -17,7 +17,256 @@ module.exports = {
             ]
         },
         {
-            "The 1964 classic Rudolph The Red Nosed Reindeer was filmed in": [
+            "The 1964 classic Rudolph The Red Nosed Reindeer was filmed in. ": [
+                "Japan",
+                "United States",
+                "Finland",
+                "Germany",
+                "Canada",
+                "Norway",
+                "France"
+            ]
+        },
+        {
+            "Santas reindeer are cared for by one of the Christmas elves, what is his name?": [
+                "Wunorse Openslae",
+                "Alabaster Snowball",
+                "Bushy Evergreen",
+                "Pepper Minstix"
+            ]
+        },
+        {
+            "If all of Santas reindeer had antlers while pulling his Christmas sleigh, they would all be. ": [
+                "Girls",
+                "Boys",
+                "Girls and boys",
+                "No way to tell"
+            ]
+        },
+        {
+            "What do Reindeer eat?": [
+                "Lichen",
+                "Grasses",
+                "Leaves",
+                "Berries"
+            ]
+        },
+        {
+            "What of the following is not true?": [
+                "Caribou live on all continents",
+                "Both reindeer and Caribou are the same species",
+                "Caribou are bigger than reindeer",
+                "Reindeer live in Scandinavia and Russia"
+            ]
+        },
+        {
+            "In what year did Rudolph make his television debut?": [
+                "1964",
+                "1979",
+                "2000",
+                "1956"
+            ]
+        },
+        {
+            "Who was the voice of Rudolph in the 1964 classic?": [
+                "Billie Mae Richards",
+                "Burl Ives",
+                "Paul Soles",
+                "Lady Gaga"
+            ]
+        },
+        {
+            "In 1939 what retailer used the story of Rudolph the Red Nose Reindeer?": [
+                "Montgomery Ward",
+                "Sears",
+                "Macys",
+                "Kmart"
+            ]
+        },
+        {
+            "Santa\'s reindeer named Donner was originally named what?": [
+                "Dunder",
+                "Donny",
+                "Dweedle",
+                "Dreamy"
+            ]
+        },
+        {
+            "Who invented the story of Rudolph?": [
+                "Robert May",
+                "Johnny Marks",
+                "Santa",
+                "J.K. Rowling"
+            ]
+        },
+        {
+            "In what location will you not find reindeer?": [
+                "North Pole",
+                "Lapland",
+                "Korvatunturi mountain",
+                "Finland"
+            ]
+        },
+        {
+            "What Makes Santa\'s Reindeer Fly?": [
+                "Magical Reindeer Dust",
+                "Fusion",
+                "Amanita muscaria",
+                "Elves"
+            ]
+        },
+        {
+            "Including Rudolph, how many reindeer hooves are there?": [
+                "36",
+                "24",
+                "16",
+                "8"
+            ]
+        },
+        {
+            "Santa only has one female reindeer. Which one is it?": [
+                "Vixen",
+                "Clarice",
+                "Cupid",
+                "Cupid"
+            ]
+        },
+        {
+            "In the 1964 classic Rudolph The Red Nosed Reindeer, what was the snowman narrators name?": [
+                "Sam",
+                "Frosty",
+                "Burl",
+                "Snowy"
+            ]
+        },
+        {
+            "What was Rudolph\'s father\'s name?": [
+                "Donner",
+                "Dasher",
+                "Blixen",
+                "Comet"
+            ]
+        },
+        {
+            "In the 1964 movie, What was the name of the coach of the Reindeer Games?": [
+                "Comet",
+                "Blixen",
+                "Donner",
+                "Dasher"
+            ]
+        },
+        {
+            "In the 1964 movie, what is the name of the deer that Rudolph befriends at the reindeer games?": [
+                "Fireball",
+                "Clarice",
+                "Jumper",
+                "Vixen"
+            ]
+        },
+        {
+            "In the 1964 movie, How did Donner, Rudolph\'s father, try to hide Rudolph\'s nose?": [
+                "Black mud",
+                "Bag",
+                "Pillow case",
+                "Sock"
+            ]
+        },
+        {
+            "In the 1964 movie, what does the Misfit Elf want to be instead of a Santa Elf?": [
+                "Dentist",
+                "Reindeer",
+                "Toy maker",
+                "Candlestick maker"
+            ]
+        },
+        {
+            "In the 1964 movie,what was the Bumble\'s one weakness?": [
+                "Could not swim",
+                "Always hungry",
+                "Candy canes",
+                "Cross eyed"
+            ]
+        },
+        {
+            "In the 1964 movie, what is Yukon Cornelius really in search of?": [
+                "Peppermint",
+                "Gold",
+                "India",
+                "Polar Bears"
+            ]
+        },
+        {
+            "In the 1964 movie, why is the train on the Island of Misfit Toys?": [
+                "Square wheels",
+                "No Engine",
+                "Paint does not match",
+                "It does not toot"
+            ]
+        },
+        {
+            "In the 1964 movie, what is the name of the Jack in the Box?": [
+                "Charlie",
+                "Sam",
+                "Billy",
+                "Jack"
+            ]
+        },
+        {
+            "In the 1964 movie, why did Santa Claus almost cancel Christmas?": [
+                "Storm",
+                "No snow",
+                "No toys",
+                "The Reindeer were sick"
+            ]
+        },
+        {
+            "In the 1964 movie, what animal noise did the elf make to distract the Bumble?": [
+                "Oink",
+                "Growl",
+                "Bark",
+                "Meow"
+            ]
+        },
+        {
+            "In the 1964 movie, what is the name of the prospector?": [
+                "Yukon Cornelius",
+                "Slider Sam",
+                "Bumble",
+                "Jack"
+            ]
+        },
+        {
+            "How far do reindeer travel when they migrate?": [
+                "3000 miles",
+                "700 miles",
+                "500 miles",
+                "0 miles"
+            ]
+        },
+        {
+            "How fast can a reindeer run?": [
+                "48 miles per hour",
+                "17 miles per hour",
+                "19 miles per hour",
+                "14 miles per hour",
+                "52 miles per hour",
+                "41 miles per hour"
+            ]
+        }
+    ],
+    "QUESTIONS_EN_US" : [
+        {
+            "Reindeer have very thick coats, how many hairs per square inch do they have?": [
+                "13,000",
+                "1,200",
+                "5,000",
+                "700",
+                "1,000",
+                "120,000"
+            ]
+        },
+        {
+            "The 1964 classic Rudolph The Red Nosed Reindeer was filmed in. ": [
                 "Japan",
                 "United States",
                 "Finland",
@@ -254,9 +503,9 @@ module.exports = {
             ]
         }
     ],
-    "QUESTIONS_EN_US" : [
+    "QUESTIONS_DE_DE" : [
         {
-            "Reindeer have very thick coats, how many hairs per square inch do they have?": [
+            "Rentiere haben ein sehr dickes Fell. Wie viele Haare pro Quadratzentimeter haben sie?": [
                 "13,000",
                 "1,200",
                 "5,000",
@@ -266,50 +515,50 @@ module.exports = {
             ]
         },
         {
-            "The 1964 classic Rudolph The Red Nosed Reindeer was filmed in": [
+            "Der Klassiker aus dem Jahr 1964, Rudolph mit der roten Nase, wurde gedreht in. ": [
                 "Japan",
-                "United States",
-                "Finland",
-                "Germany",
-                "Canada",
-                "Norway",
-                "France"
+                "USA",
+                "Finnland",
+                "Deutschland",
+                "Kanada",
+                "Norwegen",
+                "Frankreich"
             ]
         },
         {
-            "Santas reindeer are cared for by one of the Christmas elves, what is his name?": [
+            "Um die Rentiere des Weihnachtsmanns kümmert sich eine der Weihnachtselfen. Wie heißt sie?": [
                 "Wunorse Openslae",
                 "Alabaster Snowball",
                 "Bushy Evergreen",
-                "Pepper Minstix"
+                "Pfeffer Minstix"
             ]
         },
         {
-            "If all of Santas reindeer had antlers while pulling his Christmas sleigh, they would all be": [
-                "Girls",
-                "Boys",
-                "Girls and boys",
-                "No way to tell"
+            "Wenn alle Rentiere des Weihnachtsmanns Geweihe hätten, während sie seinen Weihnachtsschlitten ziehen, wären sie alle. ": [
+                "Weiblich",
+                "Männlich",
+                "Weiblich und männlich",
+                "Kann man nicht sagen"
             ]
         },
         {
-            "What do Reindeer eat?": [
-                "Lichen",
-                "Grasses",
-                "Leaves",
-                "Berries"
+            "Was essen Rentiere?": [
+                "Flechten",
+                "Gras",
+                "Blätter",
+                "Beeren"
             ]
         },
         {
-            "What of the following is not true?": [
-                "Caribou live on all continents",
-                "Both reindeer and Caribou are the same species",
-                "Caribou are bigger than reindeer",
-                "Reindeer live in Scandinavia and Russia"
+            "Welche Aussage ist nicht richtig?": [
+                "Karibus leben auf allen Kontinenten",
+                "Karibus und Rentiere gehören derselben Gattung an ",
+                "Karibus sind größer als Rentiere",
+                "Rentiere leben in Skandinavien und Russland"
             ]
         },
         {
-            "In what year did Rudolph make his television debut?": [
+            "In welchem Jahr kam Rudolph ins Fernsehen?": [
                 "1964",
                 "1979",
                 "2000",
@@ -317,7 +566,7 @@ module.exports = {
             ]
         },
         {
-            "Who was the voice of Rudolph in the 1964 classic?": [
+            "Wer war der Sprecher für Rudolph im klassischen Film aus dem Jahr 1964?": [
                 "Billie Mae Richards",
                 "Burl Ives",
                 "Paul Soles",
@@ -325,7 +574,7 @@ module.exports = {
             ]
         },
         {
-            "In 1939 what retailer used the story of Rudolph the Red Nose Reindeer?": [
+            "Welche Handelskette verwendete 1939 die Geschichte von Rudolph mit der roten Nase?": [
                 "Montgomery Ward",
                 "Sears",
                 "Macys",
@@ -333,7 +582,7 @@ module.exports = {
             ]
         },
         {
-            "Santa\'s reindeer named Donner was originally named what?": [
+            "Wie hieß das Rentier des Weihnachtsmanns namens Donner ursprünglich?": [
                 "Dunder",
                 "Donny",
                 "Dweedle",
@@ -341,7 +590,7 @@ module.exports = {
             ]
         },
         {
-            "Who invented the story of Rudolph?": [
+            "Wer hat die Geschichte von Rudolph erfunden?": [
                 "Robert May",
                 "Johnny Marks",
                 "Santa",
@@ -349,23 +598,23 @@ module.exports = {
             ]
         },
         {
-            "In what location will you not find reindeer?": [
-                "North Pole",
-                "Lapland",
-                "Korvatunturi mountain",
-                "Finland"
+            "Wo findest du keine Rentiere?": [
+                "Nordpol",
+                "Lappland",
+                "Korvatunturi-Berge",
+                "Finnland"
             ]
         },
         {
-            "What Makes Santa\'s Reindeer Fly?": [
-                "Magical Reindeer Dust",
+            "Warum können die Rentiere des Weihnachtsmanns fliegen?": [
+                "Magischer Staub der Rentiere",
                 "Fusion",
                 "Amanita muscaria",
-                "Elves"
+                "Elfen"
             ]
         },
         {
-            "Including Rudolph, how many reindeer hooves are there?": [
+            "Wieviele Rentierhufe gibt es hier einschließlich Rudolph?": [
                 "36",
                 "24",
                 "16",
@@ -373,15 +622,15 @@ module.exports = {
             ]
         },
         {
-            "Santa only has one female reindeer. Which one is it?": [
-                "Vixen",
+            "Der Weihnachtsmann hat nur ein weibliches Rentier. Wie heißt es?": [
+                "Blitzen",
                 "Clarice",
                 "Cupid",
                 "Cupid"
             ]
         },
         {
-            "In the 1964 classic Rudolph The Red Nosed Reindeer, what was the snowman narrators name?": [
+            "Wie war der Name des erzählenden Schneemanns im klassischen Film Rudolph mit der roten Nase aus dem Jahr 1964?": [
                 "Sam",
                 "Frosty",
                 "Burl",
@@ -389,7 +638,7 @@ module.exports = {
             ]
         },
         {
-            "What was Rudolph\'s father\'s name?": [
+            "Wie hieß der Vater von Rudolph?": [
                 "Donner",
                 "Dasher",
                 "Blixen",
@@ -397,7 +646,7 @@ module.exports = {
             ]
         },
         {
-            "In the 1964 movie, What was the name of the coach of the Reindeer Games?": [
+            "Wie war der Name des Trainers der Rentierspiele im klassischen Film aus dem Jahr 1964?": [
                 "Comet",
                 "Blixen",
                 "Donner",
@@ -405,7 +654,7 @@ module.exports = {
             ]
         },
         {
-            "In the 1964 movie, what is the name of the deer that Rudolph befriends at the reindeer games?": [
+            "Wie war im klassichen Film aus 1964 der Name des Hirsches, mit dem sich Rudolph befreundete?": [
                 "Fireball",
                 "Clarice",
                 "Jumper",
@@ -413,47 +662,47 @@ module.exports = {
             ]
         },
         {
-            "In the 1964 movie, How did Donner, Rudolph\'s father, try to hide Rudolph\'s nose?": [
-                "Black mud",
-                "Bag",
-                "Pillow case",
-                "Sock"
+            "Wie hat der Vater von Rudolph, Donner, im Film aus dem Jahr 1964 versucht, die Nase von Rudolph zu verbergen?": [
+                "Schwarzer Schlamm",
+                "Sack",
+                "Kissenbezug",
+                "Socke"
             ]
         },
         {
-            "In the 1964 movie, what does the Misfit Elf want to be instead of a Santa Elf?": [
-                "Dentist",
-                "Reindeer",
-                "Toy maker",
-                "Candlestick maker"
+            "Was möchte die Misfit-Elfe im Film aus dem Jahr 1964 werden anstatt eine Elfe für den Weihnachtsmann?": [
+                "Zahnarzt",
+                "Rentier",
+                "Spielzeugmacher",
+                "Kerzenmacher"
             ]
         },
         {
-            "In the 1964 movie,what was the Bumble\'s one weakness?": [
-                "Could not swim",
-                "Always hungry",
-                "Candy canes",
-                "Cross eyed"
+            "Was war die einzige Schwäche von Bumble im Film aus dem Jahr 1964?": [
+                "Konnte nicht schwimmen",
+                "War immer hungrig",
+                "Zuckerstangen",
+                "Schielte"
             ]
         },
         {
-            "In the 1964 movie, what is Yukon Cornelius really in search of?": [
-                "Peppermint",
+            "Was sucht Yukon Cornelius in Wirklichkeit im Film aus dem Jahr 1964?": [
+                "Pfefferminz",
                 "Gold",
-                "India",
-                "Polar Bears"
+                "Indien",
+                "Polarbären"
             ]
         },
         {
-            "In the 1964 movie, why is the train on the Island of Misfit Toys?": [
-                "Square wheels",
-                "No Engine",
-                "Paint does not match",
-                "It does not toot"
+            "Warum befindet sich der Zug im Film aus dem Jahr 1964 auf der Insel des fehlerhaften Spielzeugs?": [
+                "Viereckige Räder",
+                "Keine Dampfmaschine",
+                "Farbe stimmt nicht",
+                "Pfeift nicht"
             ]
         },
         {
-            "In the 1964 movie, what is the name of the Jack in the Box?": [
+            "Wie lautet der Name des Schachtelmännchens im Film aus dem Jahr 1964?": [
                 "Charlie",
                 "Sam",
                 "Billy",
@@ -461,23 +710,23 @@ module.exports = {
             ]
         },
         {
-            "In the 1964 movie, why did Santa Claus almost cancel Christmas?": [
-                "Storm",
-                "No snow",
-                "No toys",
-                "The Reindeer were sick"
+            "Warum hat der Weihnachtsmann im Film aus dem Jahr 1964 Weihnachten beinahe abgesagt?": [
+                "Sturm",
+                "Kein Schnee",
+                "Kein Spielzeug",
+                "Die Rentiere waren krank"
             ]
         },
         {
-            "In the 1964 movie, what animal noise did the elf make to distract the Bumble?": [
+            "Welches tierische Geräusch machte die Elfe im Film aus dem Jahr 1964, um den Bumble abzulenken?": [
                 "Oink",
-                "Growl",
-                "Bark",
-                "Meow"
+                "Knurr",
+                "Wauwau",
+                "Miau"
             ]
         },
         {
-            "In the 1964 movie, what is the name of the prospector?": [
+            "Wie lautet der Name des Goldsuchers im Film aus dem Jahr 1964?": [
                 "Yukon Cornelius",
                 "Slider Sam",
                 "Bumble",
@@ -485,21 +734,21 @@ module.exports = {
             ]
         },
         {
-            "How far do reindeer travel when they migrate?": [
-                "3000 miles",
-                "700 miles",
-                "500 miles",
-                "0 miles"
+            "Wie weit ziehen Rentiere auf ihren Wanderungen?": [
+                "4800 km",
+                "1100 km",
+                "800 km",
+                "0 km"
             ]
         },
         {
-            "How fast can a reindeer run?": [
-                "48 miles per hour",
-                "17 miles per hour",
-                "19 miles per hour",
-                "14 miles per hour",
-                "52 miles per hour",
-                "41 miles per hour"
+            "Wie schnell läuft ein Rentier?": [
+                "77 km pro Stunde",
+                "27 km pro Stunde",
+                "30 km pro Stunde",
+                "22 km pro Stunde",
+                "83 km pro Stunde",
+                "65 km pro Stunde"
             ]
         }
     ]
