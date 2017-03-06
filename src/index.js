53ed8782e1e7402312874abfc4601fccbb215ad7
sethten <sethten@users.noreply.github.com>
2017-03-06 12:13:40 -0500

    Remove duplicate APP_ID definition

--- src/index.js
+++ src/index.js
@@ -84,7 +84,6 @@ var languageString = {
 };
 
 var Alexa = require("alexa-sdk");
-var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
