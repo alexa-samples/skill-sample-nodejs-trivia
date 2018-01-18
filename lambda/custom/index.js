a6ba1ce4a897114d172968e2c300cdfa129ee7a9
kellechu <github@kellechu.com>
2018-01-17 22:22:54 -0700

    We changed game name.

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -32,7 +32,7 @@ const languageString = {
     "en": {
         "translation": {
             "QUESTIONS": questions["QUESTIONS_EN_US"],
-            "GAME_NAME": "Reindeer Trivia", // Be sure to change this for your skill.
+            "GAME_NAME": "Dunkin Science Trivia", // Be sure to change this for your skill.
             "HELP_MESSAGE": "I will ask you %s multiple choice questions. Respond with the number of the answer. " +
                 "For example, say one, two, three, or four. To start a new game at any time, say, start game. ",
             "REPEAT_QUESTION_MESSAGE": "To repeat the last question, say, repeat. ",
