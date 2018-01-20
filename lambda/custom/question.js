d2077b05dcfaaa7e11c7a6eeb5150ac9a9692427
kellechu <github@kellechu.com>
2018-01-19 18:21:06 -0700

    Populating question data

--- lambda/custom/question.js
+++ lambda/custom/question.js
@@ -7,11 +7,211 @@ module.exports = {
      */
     QUESTIONS_EN_US: [
         {
-            'Which science term is defined as reducing size by squeezing': [
+            'Which science term is defined as reducing size by squeezing?': [
                 'Compression',
-                'Squish',
-                'Smush',
-                'Pinch',
+                'Force',
+                'Gravity',
+                'Load',
+            ]
+        },
+        {
+            'Which science term is defined as reducing length by bending over?': [
+                'Folding',
+                'Shrinking',
+                'Replicating',
+                'Pinching',
+            ]
+        },
+        {
+            'Which science term means to run an experiment again and get the same result?': [
+                'Replicate',
+                'Valid',
+                'Trial',
+                'Simulate',
+            ]
+        },
+        {
+            'What term describes one time through a procedure?': [
+                'Trial',
+                'Repeatable',
+                'Iteration',
+                'Simulation',
+            ]
+        },
+        {
+            'What term describes a push or pull on an object?': [
+                'Force',
+                'Pressure',
+                'Compression',
+                'Gravity',
+            ]
+        },
+        {
+            'What are goals that must be satisfied to successfully achieve a challenge?': [
+                'Criteria',
+                'Constraints',
+                'Claim',
+                'Recommendation',
+            ]
+        },
+        {
+            'What are factors that limit how you can solve a problem?': [
+                'Constraints',
+                'Criteria',
+                'Variable',
+                'Recommendation',
+            ]
+        },
+        {
+            'What is the amount of space that something takes up?': [
+                'Volume',
+                'Matter',
+                'Container',
+                'Model',
+            ]
+        },
+        {
+            'What is defined as anything that takes up space and has mass?': [
+                'Matter',
+                'Volume',
+                'Molecules',
+                'Structure',
+            ]
+        },
+        {
+            'Which science term is defined as a small particle of matter?': [
+                'Atom',
+                'Molecule',
+                'Mineral',
+                'Compound',
+            ]
+        },
+        {
+            'What is a group of atoms bonded together, representing the smallest fundamental unit of a chemical compound that can take part in a chemical reaction?': [
+                'Molecules',
+                'Atoms',
+                'Mineral',
+                'Model',
+            ]
+        },
+        {
+            'Which science term is defined as the force that attracts a body toward any other physical body having mass?': [
+                'Gravity',
+                'Compression',
+                'Strong Force',
+                'Pull',
+            ]
+        },
+        {
+            'Which science term is defined as able to withstand force?': [
+                'Strong',
+                'Structure',
+                'Stable',
+                'Load',
+            ]
+        },
+        {
+            'What are structures that resist folding and compressing?': [
+                'Strong Structures',
+                'Molecules',
+                'Atoms',
+                'Stable',
+            ]
+        },
+        {
+            'What term means to be able to resist tipping over?': [
+                'Stable',
+                'Strong',
+                'Structure',
+                'Force',
+            ]
+        },
+        {
+            "What is the imaginary point on or near an object around which the object's matter is equally distributed?": [
+                'Center of mass',
+                'Stable point',
+                'Density',
+                'Satellite',
+            ]
+        },
+        {
+            'Which science term is defined as the amount of push or pull a structure has to resist?': [
+                'Load',
+                'Force',
+                'Strong',
+                'Range',
+            ]
+        },
+        {
+            'What is the display of data in which each data item is shown as an x above its value on a number line?': [
+                'Line Plot',
+                'Data Graph',
+                'Bar Graph',
+                'Variable',
+            ]
+        },
+        {
+            'What is the term to describe when someone follows the reported procedure, they get similar results?': [
+                'Repeatable',
+                'Precision',
+                'Trial',
+                'Template',
+            ]
+        },
+        {
+            'What word describes how close together the measured values are?': [
+                'Precision',
+                'Range',
+                'Variation',
+                'Line Plot',
+            ]
+        },
+        {
+            'Which science term is defined as the zone between the largest and smallest solution results?': [
+                'Range',
+                'Bar Graph',
+                'Variation',
+                'Constraints',
+            ]
+        },
+        {
+            'What term means a wide spread of data?': [
+                'Variation',
+                'Range',
+                'Precision',
+                'Trend',
+            ]
+        },
+        {
+            'Which science term is defined as a pattern or a tendency?': [
+                'Trend',
+                'Repeatable',
+                'Line Plot',
+                'Phenomenon',
+            ]
+        },
+        {
+            'What is the definition of a template?': [
+                'Pattern',
+                'Repeatable',
+                'Plan Briefing',
+                'Criterion',
+            ]
+        },
+        {
+            'What is an exploratory activity that gives a chance to become familiar with the materials or the function of the design?': [
+                'Messing About',
+                'Trial',
+                'Investigation Expo',
+                'Mechanism',
+            ]
+        },
+        {
+            'What is the way the parts of an item are put together?': [
+                'Structure',
+                'Mechanism',
+                'Template',
+                'Criteria',
             ]
         },
         {
@@ -54,5 +254,6 @@ module.exports = {
                 'Pinch',
             ]
         },
+
     ],
 };
