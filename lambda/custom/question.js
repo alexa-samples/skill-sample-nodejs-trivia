830bdc5ec82d2ac644504ca2de9d7aeae25de32d
kellechu <github@kellechu.com>
2018-01-21 19:48:59 -0700

    populating more general science questions

--- lambda/custom/question.js
+++ lambda/custom/question.js
@@ -63,7 +63,7 @@ module.exports = {
             ]
         },
         {
-            'What is the amount of space that something takes up?': [
+            'What is the amount of space a substance or object occupies?': [
                 'Volume',
                 'Matter',
                 'Container',
@@ -202,16 +202,192 @@ module.exports = {
             'What is an exploratory activity that gives a chance to become familiar with the materials or the function of the design?': [
                 'Messing About',
                 'Trial',
-                'Investigation Expo',
+                'Investigation Exposition',
                 'Mechanism',
             ]
         },
         {
-            'What is the way the parts of an item are put together?': [
+            'What describes the way parts of an item are put together?': [
                 'Structure',
-                'Mechanism',
-                'Template',
                 'Criteria',
+                'Template',
+                'Model',
+            ]
+        },
+        {
+            'What describes the way parts of an item connect and move?': [
+                'Mechanism',
+                'Structure',
+                'Template',
+                'Model',
+            ]
+        },
+        {
+            'What is the definition of phenomenon?': [
+                'Something that happens',
+                'To find the meaning of something',
+                'A pattern or tendency',
+                'A statement of what you understand',
+            ]
+        },
+        {
+            'What is a quantity whose value may vary over the course of an experiment?': [
+                'Variable',
+                'Line Graph',
+                'Bar Graph',
+                'Evidence',
+            ]
+        },
+        {
+            'Which science term is defined as finding the meaning of something?': [
+                'Interpret',
+                'Investigation',
+                'Recommendation',
+                'Plan Briefing',
+            ]
+        },
+        {
+            'Which is a statement of what you understand or a conclusion that you have reached from investigations?': [
+                'Claim',
+                'Plan Briefing',
+                'Recommendation',
+                'Evidence',
+            ]
+        },
+        {
+            'What is the definition of a fair test?': [
+                'Things that are being compared are being tested under the same conditions',
+                'In an experiment, the variables that are kept constant',
+                'A presentation of the procedure, results, and interpretations of the results from an investigation',
+                'An opportunity during the design process to share plans and get advice from others',
+            ]
+        },
+        {
+            'Which science term is defined as a presentation of the procedure, results, and interpretations of the results from an investigation?': [
+                'Investigation Exposition',
+                'Plan Briefing',
+                'Science Knowledge',
+                'Evidence',
+            ]
+        },
+        {
+            'What is the independent variable?': [
+                'The variable that the scientist intentionally changes in an experiment',
+                'The variable that is kept constant in an experiment',
+                'The variable whose value is measured in an experiment',
+                'The responding variable in an experiment',
+            ]
+        },
+        {
+            'What are the control variables?': [
+                'The variables that are kept constant in an experiment',
+                'The variables that the scientist intentionally change in an experiment',
+                'The manipulated variables in an experiment',
+                'The responding variables in an experiment',
+            ]
+        },
+        {
+            'What are the dependent variables?': [
+                'The variables whose values are measured in an experiment',
+                'The variables that are kept constant in an experiment',
+                'The variables that the scientist intentionally change in an experiment',
+                'The manipulated variables in an experiment',
+            ]
+        },
+        {
+            'Which science term is defined as the opposing push that resists the movement of an object through air?': [
+                'Drag',
+                'Displacement',
+                'Viscosity',
+                'Gravity',
+            ]
+        },
+        {
+            'What is data collected during invesigations and the apparent trends in that data?': [
+                'Evidence',
+                'Trials',
+                'Variables',
+                'Phenomena',
+            ]
+        },
+        {
+            'How do you find a ratio?': [
+                'Divide one number by another number',
+                'Multiply one number by another number',
+                'Add one number to another number',
+                'Subtract one number from another number',
+            ]
+        },
+        {
+            'Which of the following is not an expression of density?': [
+                'Dividing the volume of an object by its mass',
+                'Dividing the mass of an object by the volume',
+                'The ratio of the mass and volume of a substance',
+                'The amount of matter in an given amount of space',
+            ]
+        },
+        {
+            'Which is an example of an extensive property?': [
+                'The mass of the matter',
+                'The density of the matter',
+                'A property of the matter that does not depend on the amount of matter present in a sample',
+                'The conductivity of the matter',
+            ]
+        },
+        {
+            'Which is an example of an intensive property?': [
+                'The density of the matter',
+                'The mass of the matter',
+                'The volume of the matter',
+                'A property of the matter that depends on the amount of matter present in a sample',
+            ]
+        },
+        {
+            'To displace something is to': [
+                'replace it',
+                'compress it',
+                'drop it',
+                'control it',
+            ]
+        },
+        {
+            'Which science term is defined as the repetition to improve on the first design?': [
+                'Iteration',
+                'Model',
+                'Trial',
+                'Standardize',
+            ]
+        },
+        {
+            'What is the definition of a model?': [
+                'A representation of something',
+                'An imitation of real-life situations',
+                'A mixture that has a uniform composition of substances',
+                'A mixture that varies in composition',
+            ]
+        },
+        {
+            'Which is the definition of pressure?': [
+                'The amount of compression force acting on a substance',
+                'The opposing push that resists the movement of an object through air',
+                'The amount of gravity exerted on an object',
+                'The upward force exerted by any fluid upon a body placed in it',
+            ]
+        },
+        {
+            'What are royalties?': [
+                'Fees paid to the owner of a patent when you use it',
+                'To give credit to any scientists or engineers that you got ideas from',
+                'To encourage teamwork and combining ideas together',
+                'People from a certain lineage or ancestry',
+            ]
+        },
+        {
+            'Which science term is defined as an imitation of real-life situations?': [
+                'Simulate',
+                'Model',
+                'Criteria',
+                'Plan',
             ]
         },
         {
@@ -254,6 +430,8 @@ module.exports = {
                 'Pinch',
             ]
         },
+
+
 
     ],
 };
